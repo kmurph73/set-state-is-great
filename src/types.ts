1af2abb4f508b914b592470d5e275d3229baa761
Kyle Murphy <kmurph73@gmail.com>
2019-09-13 09:36:10 -0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -43,7 +43,7 @@ export interface Store {
   forceUpdateViaName: (store: string, name: string) => void;
 }
 
-// React's TS signature for useReducer requires a variable be passed in
+// React's TS signature for useReducer has a required argument
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts#L802
 // however, our forceUpdate expects nothing to be passed in
 // so, this is a hack to make passing in a value to useReducer optional
