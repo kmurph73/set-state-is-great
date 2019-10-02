ecafd435d2b12e2ce9b63dc5fb69f45067783151
Kyle Murphy <kmurph73@gmail.com>
2019-10-02 14:43:09 -0700

    next version of ssig; make full use of TS Generics

--- src/store.ts
+++ src/store.ts
@@ -1,237 +1,145 @@
-import {
-  StateObj,
-  ForceUpdateIfMounted,
-  QueryObject,
-  PlainObject,
-  Store,
-  StateHelpers,
-  DynamicStateHelpers,
-  DynamicQueryObject,
-} from './types';
+import React from 'react';
+import { ForceUpdateIfMounted } from './types';
+import useForceUpdateIfMounted from './useForceUpdateIfMounted';
+import useComponentId from './useComponentId';
 
-let state: StateObj;
+export default class Store<State> {
+  private state: State;
 
-interface ObjHold {
-  obj: QueryObject | DynamicQueryObject;
-  forceUpdate: ForceUpdateIfMounted;
+  private objStore: Map<
+    number,
+    {
+      store: keyof State;
+      watchAttrs?: Array<string> | null;
+      name?: string;
+      forceUpdate: ForceUpdateIfMounted;
+    }
+  >;
+
+  constructor(state: State) {
+    this.state = state;
+    this.objStore = new Map();
+  }
+
+  getStore() {
+    return this.state;
+  }
+
+  /**
+   * *assign* values to a store.  if you want to replace the object in a store, there's assignState
+   *
+   * https://github.com/kmurph73/set-state-is-great#setstate
+   *
+   * @example
+   *  store.setState('drawer', {open: true});
+   *
+   */
+
+  setState<K extends keyof State>(store: K, nextState: State[K]) {
+    const changedAttrs: Array<string> = [];
+
+    const existingState = this.state[store];
+
+    for (const attr in nextState) {
+      if (existingState[attr] !== nextState[attr]) {
+        existingState[attr] = nextState[attr];
+        changedAttrs.push(attr);
+      }
+    }
+
+    const forceUpdatesToCall: Array<ForceUpdateIfMounted> = [];
+
+    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+    for (const [id, obj] of this.objStore) {
+      if (!obj.watchAttrs || this.watchingAttrs(changedAttrs, obj.watchAttrs)) {
+        forceUpdatesToCall.push(obj.forceUpdate);
+      }
+    }
+
+    const len = forceUpdatesToCall.length;
+    for (let i = 0; i < len; i++) {
+      forceUpdatesToCall[i]();
+    }
+  }
+
+  watchingAttrs(changedAttrs: Array<string>, watchAttrs: Array<string>) {
+    for (let i = 0; i < changedAttrs.length; i++) {
+      if (watchAttrs.includes(changedAttrs[i])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  getState<K extends keyof State>(s: K) {
+    return this.state[s];
+  }
+
+  createGetState<K extends keyof State>(s: K) {
+    return () => {
+      return this.getState<K>(s);
+    };
+  }
+
+  createSetState<K extends keyof State>(s: K) {
+    return (next: State[K]) => {
+      return this.setState(s, next);
+    };
+  }
+
+  unsubscribe(id: number) {
+    this.objStore.delete(id);
+  }
+
+  subscribe<K extends keyof State, X extends keyof State[K]>(
+    id: number,
+    store: K,
+    forceUpdate: ForceUpdateIfMounted,
+    watchAttrs?: Array<X> | null,
+    name?: string,
+  ) {
+    this.objStore.set(id, {
+      store: store,
+      watchAttrs: watchAttrs as Array<string>,
+      forceUpdate: forceUpdate,
+      name: name,
+    });
+  }
+
+  useStoreState<K extends keyof State, X extends keyof State[K]>(
+    store: K,
+    watchAttrs?: Array<X> | null,
+    name?: string,
+  ) {
+    const id = useComponentId();
+    this.subscribe(id, store, useForceUpdateIfMounted(), watchAttrs, name);
+
+    React.useEffect(
+      () => (): void => {
+        this.unsubscribe(id);
+      },
+      [id],
+    );
+
+    return this.getState(store);
+  }
+
+  createUseStoreState<K extends keyof State, X extends keyof State[K]>(
+    store: K,
+    watchAttrs?: Array<X> | null,
+    name?: string,
+  ) {
+    return () => {
+      return this.useStoreState(store, watchAttrs, name);
+    };
+  }
+
+  getFns<K extends keyof State, X extends keyof State[K]>(store: K, watchAttrs?: Array<X> | null, name?: string) {
+    return {
+      useStoreState: this.createUseStoreState(store, watchAttrs, name),
+      getState: this.createGetState(store),
+      setState: this.createSetState(store),
+    };
+  }
 }
-
-interface ObjStore {
-  [s: string]: Map<QueryObject | string, ObjHold>;
-}
-
-const objStore: ObjStore = {};
-
-export const subscribe = (obj: QueryObject, forceUpdate: ForceUpdateIfMounted): void => {
-  objStore[obj.store].set(obj, { obj, forceUpdate });
-};
-
-export const unsubscribe = (obj: QueryObject): void => {
-  objStore[obj.store].delete(obj);
-};
-
-export const subscribeDynamic = (obj: DynamicQueryObject, forceUpdate: ForceUpdateIfMounted): void => {
-  objStore[obj.store].set(obj.key, { obj, forceUpdate });
-};
-
-export const unsubscribeDynamic = (key: string, store: string): void => {
-  objStore[store].delete(key);
-};
-
-const watchingAttrs = (changedAttrs: Array<string>, watchAttrs: Array<string>): boolean => {
-  for (let i = 0; i < changedAttrs.length; i++) {
-    if (watchAttrs.includes(changedAttrs[i])) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
-/**
- * get the core state object that contains all of the stores
- *
- * https://github.com/kmurph73/set-state-is-great#getfullstate
- *
- * @example
- *  store.getFullState();
- *
- */
-const getFullState = (): StateObj => state;
-
-/**
- * replace the state of a store
- *
- * rerenders components watching any of the attrs that have differing values between the previous and next state
- *
- * https://github.com/kmurph73/set-state-is-great#assignstate
- *
- * @example
- *  store.assignState('modal', {open: true, title: 'muh modal'});
- *
- */
-const assignState = (store: string, nextState: PlainObject): void => {
-  const changedAttrs: Array<string> = [];
-
-  const existingState = state[store];
-
-  const allAttrs = new Set(Object.keys(nextState).concat(Object.keys(existingState)));
-
-  allAttrs.forEach(attr => {
-    if (existingState[attr] !== nextState[attr]) {
-      changedAttrs.push(attr);
-    }
-  });
-
-  state[store] = nextState;
-
-  const forceUpdatesToCall: Array<ForceUpdateIfMounted> = [];
-
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  for (const [objOrKey, { obj, forceUpdate }] of objStore[store].entries()) {
-    if (!obj.watchAttrs || watchingAttrs(changedAttrs, obj.watchAttrs)) {
-      forceUpdatesToCall.push(forceUpdate);
-    }
-  }
-
-  const len = forceUpdatesToCall.length;
-  for (let i = 0; i < len; i++) {
-    forceUpdatesToCall[i]();
-  }
-};
-
-/**
- * *assign* values to a store.  if you want to replace the object in a store, there's assignState
- *
- * https://github.com/kmurph73/set-state-is-great#setstate
- *
- * @example
- *  store.setState('drawer', {open: true});
- *
- */
-const setState = (store: string, nextState: PlainObject): void => {
-  const changedAttrs: Array<string> = [];
-
-  const existingState = state[store];
-
-  for (const attr in nextState) {
-    if (existingState[attr] !== nextState[attr]) {
-      existingState[attr] = nextState[attr];
-      changedAttrs.push(attr);
-    }
-  }
-
-  const forceUpdatesToCall: Array<ForceUpdateIfMounted> = [];
-
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  for (const [objOrKey, { obj, forceUpdate }] of objStore[store].entries()) {
-    if (!obj.watchAttrs || watchingAttrs(changedAttrs, obj.watchAttrs)) {
-      forceUpdatesToCall.push(forceUpdate);
-    }
-  }
-
-  const len = forceUpdatesToCall.length;
-  for (let i = 0; i < len; i++) {
-    forceUpdatesToCall[i]();
-  }
-};
-
-/**
- * Access a store's state via `getState(store)`:
- *
- * https://github.com/kmurph73/set-state-is-great#getstate
- *
- * @example
- *  store.getState('drawer');
- *
- */
-export const getState = (store: string): PlainObject => state[store];
-
-const createSetState = (store: string) => (state: PlainObject): void => {
-  setState(store, state);
-};
-
-const createGetState = (store: string) => (): PlainObject => getState(store);
-
-const createAssignState = (store: string) => (state: PlainObject): void => {
-  assignState(store, state);
-};
-
-/**
- * forceUpdate this component from anywhere (if it's still mounted - if not, nothing bad happens).
- *
- * https://github.com/kmurph73/set-state-is-great#forceupdatevianame
- *
- * @example
- *  window.App.store.forceUpdateViaName('post', 'post_detail');
- *
- */
-const forceUpdateViaName = (store: string, name: string): void => {
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  for (const [objOrKey, { obj, forceUpdate }] of objStore[store].entries()) {
-    if (obj.name === name) {
-      forceUpdate();
-      return;
-    }
-  }
-};
-
-/**
- * Create the central store of your app (composed of smaller stores -
- * EG `main`, `drawer`, `home` shown below)
- *
- * https://github.com/kmurph73/set-state-is-great#creating-the-store
- *
- * @example
- *  import {createStore} from 'set-state-is-great';
- *
- *  const store = createStore({
- *    main: {viewShown: 'Home'},
- *    drawer: {open: false},
- *    home: {title: 'Home'}
- *  });
- */
-export const createStore = (initialState: PlainObject): Store => {
-  state = initialState;
-
-  for (const store in initialState) {
-    objStore[store] = new Map();
-  }
-
-  return {
-    getFullState,
-    setState,
-    getState,
-    assignState,
-    forceUpdateViaName,
-  };
-};
-
-/**
- * get setState & getState helpers
- *
- * https://github.com/kmurph73/set-state-is-great#getstatehelpers
- *
- * @example
- * import {getStateHelpers} from 'set-state-is-great';
- *
- * const {query, getState, setState} = getStateHelpers({
- *   store: 'drawer',
- *   watchAttrs: ['open']
- * });
- */
-export const getStateHelpers = (query: QueryObject): StateHelpers => ({
-  query,
-  getState: createGetState(query.store),
-  setState: createSetState(query.store),
-  assignState: createAssignState(query.store),
-});
-
-export const getDynamicStateHelpers = (query: DynamicQueryObject): DynamicStateHelpers => {
-  return {
-    state: getState(query.store),
-    getState: createGetState(query.store),
-    setState: createSetState(query.store),
-    assignState: createAssignState(query.store),
-  };
-};
--- src/types.ts
+++ src/types.ts
@@ -4,45 +4,6 @@ export interface QueryObject {
   name?: string;
 }
 
-export interface DynamicQueryObject {
-  store: string;
-  key: string;
-  getStateHelpers?: boolean;
-  watchAttrs?: Array<string> | null;
-  name?: string;
-}
-
-export interface StateObj {
-  [s: string]: PlainObject;
-}
-
-export interface PlainObject {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [s: string]: any;
-}
-
-export interface StateHelpers {
-  query: QueryObject;
-  getState: () => PlainObject;
-  setState: (state: PlainObject) => void;
-  assignState: (state: PlainObject) => void;
-}
-
-export interface DynamicStateHelpers {
-  state: PlainObject;
-  getState: (store: string) => PlainObject;
-  setState: (state: PlainObject) => void;
-  assignState: (state: PlainObject) => void;
-}
-
-export interface Store {
-  getFullState: (store: string) => StateObj;
-  setState: (store: string, nextState: PlainObject) => void;
-  getState: (store: string) => PlainObject;
-  assignState: (store: string, nextState: PlainObject) => void;
-  forceUpdateViaName: (store: string, name: string) => void;
-}
-
 // React's TS signature for useReducer has a required argument
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts#L802
 // however, our forceUpdate expects nothing to be passed in
